f51077f6555297b95896ae2c86896007143e35d7
OsamaNuserat <osamanuserat3@gmail.com>
2024-12-01 17:58:37 +0300

    add clustering

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppClusterService } from './app-cluster.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
 }
-bootstrap();
+
+AppClusterService.clusterize(bootstrap);
